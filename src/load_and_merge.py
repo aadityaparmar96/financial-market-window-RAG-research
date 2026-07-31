6eb84135efca92d5835214a631761e4d50158ee5
aadityaparmar96 <aadityaparmar9966@gmail.com>
2026-07-31 23:40:01 +0530

    col update2

--- src/load_and_merge.py
+++ src/load_and_merge.py
@@ -26,7 +26,7 @@ for file in files:
     df = pd.read_csv(path)
 
     date_col = df.columns[0]
-    value_col = df.columns[2]
+    value_col = df.columns[1]
 
     series_name = file.replace(".csv", "")
 
